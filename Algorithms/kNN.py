82e7b6fb79f31956cb147a5d70e6b2392b7aa834
Andrii Oliinyk <andriy.v.oliynyk@gmail.com>
2018-03-11 13:50:49 +0200

    added kNN to master branch

---
+++ Algorithms/kNN.py
@@ -0,0 +1,16 @@
+import numpy as np
+import operator
+
+def apply_kNN(pvX, pmDataSet, pvClasses, pK):
+    ldVotes = {}
+    for kClass in pvClasses:
+        ldVotes[kClass] = 0
+    lnDataSetSize = pmDataSet.shape[0]
+    lvDist = ((np.tile(pvX, (lnDataSetSize, 1)) - pmDataSet)**2).sum(1)**0.5
+    lvDist = lvDist.argsort()
+    for i in range(pK):
+        lkClass = pvClasses[lvDist[i]]
+        ldVotes[lkClass] = ldVotes[lkClass] + 1
+    result = max(ldVotes.items(), key=operator.itemgetter(1))[0]
+    return result
+
